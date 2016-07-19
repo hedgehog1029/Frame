cda976126b40803b8ce373135c1bcc1a522f4985
VaiTon86 <eyadlorenzo@gmail.com>
2016-07-19 20:15:12 +0200

    Added some JavaDocs to more useful classes

--- src/main/java/io/github/hedgehog1029/frame/annotations/Permission.java
+++ src/main/java/io/github/hedgehog1029/frame/annotations/Permission.java
@@ -3,6 +3,11 @@ package io.github.hedgehog1029.frame.annotations;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * Optional annotation.
+ * <p>
+ * It let you specify a required permission node for this command.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Permission {
     String value();
